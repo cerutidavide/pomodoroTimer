8d6d781746c08a4ccc75285fd118ede0fd8b5ee9
davideceruti <ceruti.davide@gmail.com>
2018-04-05 21:52:24 +0200

    versione funzionante (ma attenzione a dove metti le immagini e i souni)

--- com/ceruti/pomodoro/pomodoroMain.py
+++ com/ceruti/pomodoro/pomodoroMain.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from playsound import playsound
 import time
 
 
@@ -6,14 +7,13 @@ class Application (Frame):
     def __init__(self , master=None , initialtime=3):
         super ().__init__ (master)
 
-
         self.initialtime = initialtime
         self.remaining = 0
 
         self.topFrame = Frame (self)
         self.bottomFrame = Frame (self)
 
-        self.watchBox = Label (self.topFrame , text=self.initialtime)
+        self.watchBox = Label (self.topFrame , text=self.secToMinSec (self.initialtime))
 
         self.startButton = Button (self.bottomFrame , text='Start')
         self.startButton['command'] = self.pressStart
@@ -41,12 +41,13 @@ class Application (Frame):
         self.resetButton.pack (side="left")
         self.quitButton.pack (side="left")
         self.label1.pack ()
-
+    def secToMinSec(self , seconds=0):
+        minSecStr = str (divmod (seconds , 60)[0]) + "m " + str (divmod (seconds , 60)[1]) + "s"
+        return minSecStr
     def pressStart(self):
         self.startCountdown (self.initialtime - self.remaining)
-
     def pressStop(self):
-        if self.remaining==0:
+        if self.remaining == 0:
             pass
         else:
             previousstatus = self.remaining
@@ -60,9 +61,6 @@ class Application (Frame):
             self.quitButton.destroy ()
             self.label1.destroy ()
             self.__init__ (master=root , initialtime=previousstatus)
-
-        pass
-
     def pressReset(self):
         self.destroy ()
         self.topFrame.destroy ()
@@ -74,67 +72,47 @@ class Application (Frame):
         self.quitButton.destroy ()
         self.label1.destroy ()
         self.__init__ (master=root)
-        pass
-
-    #    self.hi_there["text"] = str (time.ctime (time.time ()))
-
     def startCountdown(self , remaining=None , stopped=False):
         if stopped is False:
             if remaining is not None:
                 self.remaining = remaining
 
             if self.remaining <= 0:
+
                 self.watchBox.configure (text="time's up!")
 
-                self.initialtime=0
+                self.initialtime = 0
                 self.label1.destroy ()
                 self.label1 = Label (image=self.image2)
+
                 self.label1.pack ()
 
+                playsound ('/tmp/nirvana.mp3')
 
             else:
-                self.watchBox.configure (text="%d" % self.remaining)
+                self.watchBox.configure (text=self.secToMinSec (self.remaining))
+
+                # self.watchBox.configure (text="%d" % self.remaining)
                 #            self.watchBox.pack ()
                 self.remaining = self.remaining - 1
                 self.after (1000 , self.startCountdown)
         else:
-            self.watchBox.configure (text="%d" % self.remaining)
-
-    #        print(str(self.config()))
-
-    def startCountdownWithStop(self , remaining=None):
-
-        if remaining is not None:
-            self.remaining = remaining
-
-        if self.remaining <= 0:
-            self.watchBox.configure (text="time's up!")
-            self.label1.destroy ()
-            self.label1 = Label (image=self.image2)
-            self.label1.pack ()
-
-
-        else:
-            self.watchBox.configure (text="%d" % self.remaining)
-            #            self.watchBox.pack ()
-            self.remaining = self.remaining - 1
-            self.after (1000 , self.startCountdownWithStop)
-
-    def say_hi(self):
-        # print("ciao")
-        self.watchBox["text"] = str (time.ctime (time.time ()))
-
+            self.watchBox.configure (text=self.secToMinSec (self.remaining))
     def closeWindow(self):
         print (str (time.ctime (time.time ())))
         root.destroy ()
 
-
 if __name__ == '__main__':
     root = Tk ()
+    # aFrame=Frame(root)
+    # aButton=Button(aFrame,text="SCHIACCIAMI")
+    # aButton.pack()
+    # aFrame.pack()
+    # root.mainloop()
+
 
     app = Application (master=root)
     app.master.title ("Pomodoro Timer")
-    #    app.master.maxsize(500,100)
     app.mainloop ()
 
-    pass
+
